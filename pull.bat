892d0ff661de7cfe7693c131d0580ae85cdae2f0
HeeftHeckerMan <mikebrouwer@outlook.com>
2021-11-05 23:27:34 +0100

    update from batch file

---
+++ pull.bat
@@ -0,0 +1,8 @@
+color 0b
+title updating...
+@echo off
+cls
+git pull
+echo.
+echo DONE!! POGGERS!!!
+pause>nul
---
+++ push.bat
@@ -0,0 +1,10 @@
+@echo off
+color 0b
+title pushing changes
+cls
+git add -A
+git commit -m "update from batch file"
+git push
+echo.
+echo DONE!!! POGGERS!??
+pause>nul
